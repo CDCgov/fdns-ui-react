d7e30c3df6a37029f336a57547ecc9a2dd85a148
Drew Morris <dhmorris@gmail.com>
2018-09-18 19:06:10 -0400

    Fixing tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,6 +3,12 @@ import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json';
 
+// Importing all fas icons from FA
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+library.add(fas, far);
+
 // React 16 Enzyme adapter
 Enzyme.configure({ adapter: new Adapter() });
 
