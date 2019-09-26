fd0c6a67f5309fb569f8406f3205a754bcb3f955
Jared Trotter <hello@jaredtrotter.com>
2019-09-26 15:58:11 -0400

    Added colors, and improvements
    Work on: SuperButton, SuperGrid, Container, Layout Styleing, Colors, Configs

--- src/documentation/introduction.md
+++ src/documentation/introduction.md
@@ -3,7 +3,7 @@ img { display:block; margin: 0 auto; },
 h1, h2 { text-align: center; }
 </style>
 
-![Image of FDNS Logo](../src/assets/img/fdns-logo.svg)
+![Image of FDNS Logo](img/fdns-logo.svg)
 
 # FDNS UI 
 ## DESIGN & COMPONENT LIBRARY
--- styleguide.config.js
+++ styleguide.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { version } = require('./package');
 
 module.exports = {
+	assetsDir: './src/assets/',
 	styleguideComponents: {
 		Wrapper: path.join(__dirname, 'src/StyleGuideWrapper'),
 	},
@@ -37,7 +38,7 @@ module.exports = {
       ]
 		},
 		{
-			name: 'Design Library',
+			name: 'Design Guidelines',
 			description: 'Install FDNS-UI, a CDC material design inspired UI and Framework.',
       sections: [
         {
@@ -55,61 +56,76 @@ module.exports = {
       ]
 		},
 		{
-			name: 'Buttons',	
-			components: './src/components/buttons/*.js',
-		},
-		{
-			name: 'Data Display',	
-			components: './src/components/data-display/*.js',
-		},
-		{
-			name: 'Feedback',	
-			components: './src/components/feedback/*.js',
-		},
-		{
-			name: 'Filters',
-			components: './src/components/filters/*.js',
-		},
-		{
-			name: 'Layouts',
-			components: './src/components/layouts/*.js',
-		},
-		{
-			name: 'Navigation',	
-			components: './src/components/navigation/*.js',
-		},
-		{
-			name: 'Options',	
-			components: './src/components/options/*.js',
-		},
-		{
-			name: 'Search',	
-			components: './src/components/search/*.js',
-		},
-		{
-			name: 'Surfaces',
-			components: './src/components/surfaces/*.js'
-		},
-		{
-			name: 'Uploaders',
-			components: './src/components/uploaders/*.js'
-		},
-		{
-			name: 'Utils',
-			components: './src/components/utils/*.js'
+			name: 'Component Library',
+			sections: [
+				{
+					name: 'Buttons',	
+					components: './src/components/buttons/*.js',
+				},
+				{
+					name: 'Data Display',	
+					components: './src/components/data-display/*.js',
+				},
+				{
+					name: 'Feedback',	
+					components: './src/components/feedback/*.js',
+				},
+				{
+					name: 'Filters',
+					components: './src/components/filters/*.js',
+				},
+				{
+					name: 'Layouts',
+					components: './src/components/layouts/*.js',
+				},
+				{
+					name: 'Navigation',	
+					components: './src/components/navigation/*.js',
+				},
+				{
+					name: 'Options',	
+					components: './src/components/options/*.js',
+				},
+				{
+					name: 'Search',	
+					components: './src/components/search/*.js',
+				},
+				{
+					name: 'Surfaces',
+					components: './src/components/surfaces/*.js'
+				},
+				{
+					name: 'Uploaders',
+					components: './src/components/uploaders/*.js'
+				},
+				{
+					name: 'Utils',
+					components: './src/components/utils/*.js'
+				},
+			],
 		},
 	],
+
 	theme: {
     color: {
 			name: '#ffffff',
-      baseBackground: '#fdfdfc',
+			baseBackground: '#fdfdfc',
+			sidebarLinks: '#FFF;',
       sidebarBackground: '#00223D',
-      codeBackground: '#ffffff',
-			link: '#E9E8E7',
-			linkHover: '#A6D7FF',
+      codeComment: '#717376',
+      codePunctuation: '#999',
+      codeProperty: '#905',
+      codeDeleted: '#905',
+      codeString: '#690',
+      codeInserted: '#690',
+      codeOperator: '#9a6e3a',
+      codeKeyword: '#1673b1',
+      codeFunction: '#DD4A68',
+			codeVariable: '#e90',
+			codeBackground: '#FFFFFF;'
     },
     fontFamily: {
-      base: '"Roboto", sans-serif'
+      base: '"proxima-nova", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
     }
   },
 };
