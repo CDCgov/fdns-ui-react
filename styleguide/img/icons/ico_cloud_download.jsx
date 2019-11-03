461f77cd367a8adf0bd201543f1b247643958a04
Russell Ingram <brontosauruss@gmail.com>
2019-11-03 11:16:42 -0500

    Updating styleguidist to 10.1.0

---
+++ styleguide/img/icons/ico_cloud_download.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const SvgIcoCloudDownload = props => (
+  <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}>
+    <g fill="none" fillRule="evenodd">
+      <path d="M-1-1h26.4v26.4H-1z" />
+      <path
+        d="M19.546 11.301l-.752-.05-.152-.737C18.044 7.617 15.363 5.49 12.2 5.49c-2.484 0-4.717 1.322-5.836 3.373l-.25.458-.52.052c-2.559.26-4.504 2.315-4.504 4.752 0 2.629 2.26 4.785 5.06 4.785h13.108c2.247 0 4.052-1.723 4.052-3.823 0-1.991-1.635-3.645-3.764-3.786z"
+        stroke="#3E4144"
+        strokeWidth={1.98}
+      />
+      <path
+        fill="#3E4144"
+        fillRule="nonzero"
+        d="M16.05 11.711L12.2 16.6l-3.85-4.889h2.31V7.8h3.08v3.911z"
+      />
+    </g>
+  </svg>
+);
+
+export default SvgIcoCloudDownload;
+
